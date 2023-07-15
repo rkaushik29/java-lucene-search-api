0f01268ed77ca6e979adfc3733bb8898a3451670
r3kaushi <r3kaushi@uwaterloo.ca>
2023-07-15 08:39:32 +0200

    final

--- scripts/convert_index.sh
+++ scripts/convert_index.sh
@@ -3,5 +3,5 @@
 ciff_name="$1"
 codec="${2:-[CODEC]}"
 
-cd ../ciff-lucene-importer/
+cd ../ciff-lucene-converter/
 mvn compile exec:java -Dexec.mainClass="nl.ru.ciffimporter.CiffImporter" -Dexec.args="../resources/$1.ciff ../resources/$1/ $2"
